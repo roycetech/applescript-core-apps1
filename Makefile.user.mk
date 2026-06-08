638ff7752fba9750f9aa6befd4c1893da7b7d4aa
Royce Remulla <royce.com@gmail.com>
2026-06-08 06:46:17 -0700

    🏗 build: Fix invalid reference

--- Makefile.user.mk
+++ Makefile.user.mk
@@ -7,6 +7,5 @@ SCRIPT_LIBRARY_PATH := "$(HOME)/Library/Script\ Libraries"
 include Makefile.core.mk
 include Makefile.app-common.mk
 include Makefile.app1.mk
-include Makefile.app3.mk
 include Makefile.test.mk
 include Makefile.extra.mk
